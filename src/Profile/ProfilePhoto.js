b81f044d89c07e877db3f8e1737141838cd26217
Feresbl <ramzib288@gmail.com>
2023-06-18 10:55:47 +0100

    push

--- src/Profile/ProfilePhoto.js
+++ src/Profile/ProfilePhoto.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import photo from './photo.jpg';
 
 function ProfilePhoto() {
   return (
     <div>
-      <img src="https://scontent.ftun2-2.fna.fbcdn.net/v/t39.30808-6/276127264_1295016271007339_7235279449260857480_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=wxn3Z-nNTUIAX8JRdf0&_nc_ht=scontent.ftun2-2.fna&oh=00_AfAFtX1uCA0qgtZqWVE9wZxRvjg3QZmSlQS_gzqOdH85QA&oe=64247B35" alt="Profile" />
+      <img src={photo} alt="Profile" />
     </div>
   );
 }
